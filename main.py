9e3e2ec2d56e440a9fcc22a1190b529dca36bc8f
Dhruv Rana <dhruvrana2001@gmail.com>
2022-07-22 00:39:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,51 +11,46 @@ from vmdpy import VMD
 import sys, os, os.path
 from sklearn.decomposition import PCA
 
+N=50000
 input_filename = 'input.wav'
-if input_filename[-3:] != 'wav':
-    print('WARNING!! Input File format should be *.wav')
-    sys.exit()
-samrate,data = wavfile.read(str('./' + input_filename))
-np.savetxt('out.csv',data,delimiter=",")
+samrate,data = wavfile.read(str('./'+input_filename))
 df=pd.DataFrame(data)
-f2=df.iloc[:,0]
-f=df.iloc[:,1]
-alpha = 2000       # moderate bandwidth constraint
-tau = 0           # noise-tolerance (no strict fidelity enforcement)
-K = 5              # 3 modes
-DC = 0             # no DC part imposed
-init = 1           # initialize omegas uniformly
-tol = 1e-7
+sig1=df.iloc[:,0]
+sig2=df.iloc[:,1]
+alpha=2000       # moderate bandwidth constraint
+tau=0           # noise-tolerance (no strict fidelity enforcement)
+K=5              # 5 modes
+DC=0             # no DC part imposed
+init=1           # initialize omegas uniformly
+tol=1e-7
 l=len(f)
-k=l//40000
-final=pd.DataFrame()
-final1=pd.DataFrame()
+k=l//N
+vmd1=pd.DataFrame()
+vmd2=pd.DataFrame()
 for x in range(k+1):
 	if x==k:
-		temp=f2[k*40000:l,]
-		temp2=f[k*40000:l,]
-		u, u_hat, omega = VMD(temp, alpha, tau, K, DC, init, tol)
-		u1, u_hat1, omega1 = VMD(temp2, alpha, tau, K, DC, init, tol)
+		temp1=sig1[k*N:l,]
+		temp2=sig2[k*N:l,]
+		u,u_hat,omega=VMD(temp1,alpha,tau,K,DC,init,tol)
+		u1,u_hat1,omega1=VMD(temp2,alpha,tau,K,DC,init,tol)
 		u=u.transpose()
 		u1=u1.transpose()
-		final=pd.concat([final,pd.DataFrame(u)],axis=0)
-		final1=pd.concat([final1,pd.DataFrame(u1)],axis=0)
-		print(final.shape)
+		vmd1=pd.concat([vmd1,pd.DataFrame(u)],axis=0)
+		vmd2=pd.concat([vmd2,pd.DataFrame(u1)],axis=0)
+		print(vmd1.shape)
 	else:
-		temp=f2.iloc[(x*40000):(40000*(x+1)),]
-		temp2=f.iloc[(x*40000):(40000*(x+1)),]
-		u, u_hat, omega = VMD(temp, alpha, tau, K, DC, init, tol)
-		u1, u_hat1, omega1 = VMD(temp2, alpha, tau, K, DC, init, tol)
+		temp1=sig1[(x*N):(N*(x+1)),]
+		temp2=sig2[(x*N):(N*(x+1)),]
+		u,u_hat,omega=VMD(temp1,alpha,tau,K,DC,init,tol)
+		u1,u_hat1,omega1=VMD(temp2,alpha,tau,K,DC,init,tol)
 		u=u.transpose()
 		u1=u1.transpose()
-		final=pd.concat([final,pd.DataFrame(u)],axis=0)
-		final1=pd.concat([final1,pd.DataFrame(u1)],axis=0)
-		print(final.shape)
-df=final
-df2=final1
+		vmd1=pd.concat([vmd1,pd.DataFrame(u)],axis=0)
+		vmd2=pd.concat([vmd2,pd.DataFrame(u1)],axis=0)
+		print(vmd1.shape)
 pca = PCA(n_components=5)
-Xt = pca.fit_transform(df)
-Xt2 = pca.fit_transform(df2)
+Xt = pca.fit_transform(vmd1)
+Xt2 = pca.fit_transform(vmd2)
 fcol=Xt[:,0]
 scol=Xt[:,1]
 msum=fcol+scol
